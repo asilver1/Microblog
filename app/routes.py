4f1d9e84023d232bdd99de98cff27f68716cede2
adamd <adam.david.silver@gmail.com>
2018-01-23 10:26:11 -0500

    First commit

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',title='Home',user=user)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            return redirect(url_for('login'))
+            flash('Invalid username or password')
+        login_user(user,remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html',title='Sign In',form=form)
